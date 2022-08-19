006952ad59b4d8d4048e0fe90a8aa72977b631b3
Francisco Agosto <fco@agostof.com>
2022-08-19 14:15:33 -0400

    export script with generated README rename files, update hub configs

--- ilci_hub_scripts/export_envs.sh
+++ ilci_hub_scripts/export_envs.sh
@@ -1,7 +1,17 @@
 #!/bin/bash
 
 HUB=$1
-# ignore $1 after this 
+OUT_DIR=$1
+SCRIPT_DIR=$(dirname $(readlink -e $0))
+echo "Exporting environment for: ${HUB}"
+echo "Files will be saved to: ${OUT_DIR}"
+if [ ! -d ${OUT_DIR} ]
+then
+  mkdir -v ${OUT_DIR}
+fi
+pushd ${OUT_DIR}
+
+# ignore $1 after this
 shift
 
 # #======= SAVE environment settings
@@ -20,9 +30,28 @@ echo "Saving Python package list..."
 pip freeze > user-requirements-${HUB}.txt
 
 echo "Saving R package list..."
-Rscript $(dirname $0)/save_installed_R_packages_to_list.R user-r-packages-${HUB}.txt
+Rscript ${SCRIPT_DIR}/save_installed_R_packages_to_list.R user-r-packages-${HUB}.txt
 
 echo "Export conda base env..."
 conda env export --name=base > user-condaenv-${HUB}.yml
 conda deactivate
 
+# create basic README file 
+#
+cat << EOF > README.md
+# TLJH Configuration for ${HUB}
+
+## Overview
+
+The files on this directory contain informatino about the **${HUB}** JupyterHub environment created with TLJH. In addition to the TLJH \`base\` and \`hub\` environments the individual package listing for both R and Python are stored.
+
+## Environment Files
+\`\`\`
+${HUB}
+├── hub-requirements-${HUB}.txt   <- TLJH "hub" python venv packages.
+├── user-condaenv-${HUB}.yml      <- Conda packages installed.
+├── user-requirements-${HUB}.txt  <- TLJH "user" conda-env python packages.
+├── user-r-packages-${HUB}.txt    <- R packages names only
+└── user-r-packages-${HUB}.w_version.tsv  <- R packages with versions.
+\`\`\`
+EOF
