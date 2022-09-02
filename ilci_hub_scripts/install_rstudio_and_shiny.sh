3dceabdfbbf2d46b5bc3f7231cb41c8b887feb8d
Francisco Agosto <fco@agostof.com>
2022-09-02 11:19:10 -0400

    R studio & R shinny helper script

---
+++ ilci_hub_scripts/install_rstudio_and_shiny.sh
@@ -0,0 +1,67 @@
+RSTUDIO
+# =====================    RSTUDIO   ================================
+# RSTUDIO installation
+sudo apt-get install gdebi-core
+#wget https://download2.rstudio.org/server/bionic/amd64/rstudio-server-2021.09.1-372-amd64.deb
+wget https://download2.rstudio.org/server/bionic/amd64/rstudio-server-2021.09.2-382-amd64.deb
+gdebi --non-interactive rstudio-server-2021.09.2-382-amd64.deb
+
+# check logs
+# journalctl -u rstudio-server
+
+# create configration
+cat > /etc/rstudio/rserver.conf <<EOF
+# add RServer config here
+# Server Configuration File
+#server-user=root
+#server-user=rstudio-server
+rsession-ld-library-path=/opt/tljh/user/lib:/opt/tljh/user/lib/R/lib
+#rsession-which-r=/opt/tljh/user/bin/R
+rsession-which-r=/opt/tljh/user/lib/R/bin/R
+#auth-required-user-group=rstudio_users
+#
+www-port=82
+# www-address=127.0.0.1
+#server-app-armor-enabled=0
+#server-data-dir=/var/run/rstudio-server
+server-set-umask=0
+
+EOF
+
+sleep 5
+systemctl restart rstudio-server
+
+# Notes: On Rstudio-server install 
+# If the
+# the Rstudio instalaltion should create both (when the server is run):
+# /var/lib/rstudio-server
+# /var/run/rstudio-server
+# the permission for /var/run/rstudio-server should be set (usually done when service install first time)
+# sudo mkdir -pv /var/run/rstudio-server/rstudio-rsession/
+# sudo chmod 777 -Rv /var/run/rstudio-server/rstudio-rsession
+# sudo chmod +t -Rv /var/run/rstudio-server/rstudio-rsession
+
+# Verify Rstudio installation with:
+# sudo rstudio-server verify-installation
+# If the R studio server does not point to a valid R installation it will fail
+
+# RSHINY Installation
+wget https://download3.rstudio.org/ubuntu-14.04/x86_64/shiny-server-1.5.17.973-amd64.deb
+gdebi --non-interactive shiny-server-1.5.17.973-amd64.deb
+
+
+# . /opt/tljh/user/bin/activate
+pip install jupyter-shiny-proxy
+pip install jupyter-rsession-proxy
+
+
+# ### would this  config work for RSTUDIO
+# hub:
+#   extraConfig:
+#     jupyterlab: |
+#       c.Spawner.cmd = ['jupyter-labhub']
+# singleuser:
+#   #defaultUrl: "/lab"
+#   defaultUrl: "/rstudio"
+
+
